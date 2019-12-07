5bd6dc595bef803b2b2c912794f11bc65d8e13ae
atian7064 <atian7064@gmail.com>
2019-12-07 15:19:13 -0800

    Added functions

--- compare.py
+++ compare.py
@@ -1,35 +1,51 @@
-##fib numbers
-n1 = 0
-n2 = 1
-count = 0
-number = [0,1]
+def printFibList():
+   print ("List of the first 20 fibonacci numbers:", number)
+def printTriList():
+   print("List of the first 20 triangular numbers:", trinumber)
+def printSumFib():
+   print("The sum of the first 20 fibonacci numbers is", numberSum)
+def printSumTri():
+   print("The sum of the first 20 triangular numbers is", trinumberSum)
+def printDiff():
+   print("The difference between the fibonacci and triangular numbers is", numberSum - trinumberSum)
 
 
-while count < 18:
-   nth = n1 + n2
-   n1 = n2
-   n2 = nth
-   number.append(nth)
-   count += 1
 
-numberSum = sum(number)
-##tri numbers
-count = 2
-trinumber = [1]
-x = 1
 
-while count < 21:
-   x = x + count
-   trinumber.append(x) 
-   count += 1
 
-trinumberSum = sum(trinumber)
+def fibonacci():
+   count = 0
+   n1 = 0
+   n2 = 1
+   while count < 18:
+      nth = n1 + n2
+      n1 = n2
+      n2 = nth
+      number.append(nth)
+      count += 1
+   
+   return sum(number)
+
+
+def triangular():
+   count = 2
+   x = 1
+   while count < 21:
+      x = x + count
+      trinumber.append(x) 
+      count += 1
+  
+   return sum(trinumber)
 
 
 
 ##compare
-print("List of the first 20 fibonacci numbers:", number)
-print("List of the first 20 triangular numbers:", trinumber)
-print("The sum of the first 20 fibonacci numbers is", numberSum)
-print("The sum of the first 20 triangular numbers is", trinumberSum)
-print("The difference between the fibonacci and triangular numbers is", numberSum - trinumberSum)
+number = [0,1]
+trinumber = [1]
+trinumberSum = triangular()
+numberSum = fibonacci()
+printFibList()
+printTriList()
+printSumFib()
+printSumTri()
+printDiff()
